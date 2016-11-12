9792a7232ba964b161f3891f8e3298e09d22fc80
Mikola <k.suxina@yandex.ua>
2016-11-12 19:15:13 +0200

    Додана інформація про версію за допомогою Git

--- app/src/main/java/com/beliyvlastelin/snakes/MenuActivity.java
+++ app/src/main/java/com/beliyvlastelin/snakes/MenuActivity.java
@@ -1,11 +1,14 @@
 package com.beliyvlastelin.snakes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
     SharedPreferences userPref;
@@ -21,11 +24,9 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_menu);
         userName = (TextView) findViewById(R.id.menu_user_name);
         loadUserInfo();
-        if(USER_NAME==null)
-        {
-            startActivityForResult(new Intent(this,LoginActivity.class),REQUEST_CODE);
-        }
-        else
+        if (USER_NAME == null) {
+            startActivityForResult(new Intent(this, LoginActivity.class), REQUEST_CODE);
+        } else
             userName.setText(USER_NAME);
 
     }
@@ -38,7 +39,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK){
+        if (resultCode == RESULT_OK) {
             USER_NAME = data.getStringExtra(Constants.USER_NAME_KEY);
             userName.setText(USER_NAME);
         }
@@ -46,15 +47,25 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.menu_play:{
-                startActivity(new Intent(this,SelectRoomActivity.class));
+        switch (v.getId()) {
+            case R.id.menu_play: {
+                startActivity(new Intent(this, SelectRoomActivity.class));
                 break;
             }
-            case R.id.menu_exit:{
+            case R.id.menu_exit: {
                 finish();
                 break;
             }
+            case R.id.menu_info: {
+                createDialogInfo();
+            }
         }
+    }
+
+    private void createDialogInfo() {
+        new AlertDialog.Builder(this)
+                .setMessage("Version: " + BuildConfig.GitVersion)
+                .setTitle("Info").show();
+
     }
 }
